58e46c06bd924f4364bd609a674001d8464db12f
Brenna Fisher <2.bfisher@gmail.com>
2024-03-05 21:51:53 -0700

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import collegeData from './CollegeBasketballTeams.json';
+
+interface CollegeProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+const colleges: CollegeProps[] = collegeData.teams;
+
+function Heading() {
+  return (
+    <div>
+      <h1>March Madness</h1>
+      <h2>Learn about different colleges in the NCAA!</h2>
+      <br></br>
+    </div>
+  );
+}
+
+class Card extends React.Component<CollegeProps> {
+  render() {
+    const oneCollege = this.props;
+
+    return (
+      <div>
+        <h3>College: {oneCollege.school}</h3>
+        <h4>Mascot: {oneCollege.name}</h4>
+        <h4>
+          Location: {oneCollege.city}, {oneCollege.state}
+        </h4>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+function CollegeList() {
+  return (
+    <div>
+      {colleges.map((collegeNum) => (
+        <Card {...collegeNum} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <CollegeList />
     </div>
   );
 }
